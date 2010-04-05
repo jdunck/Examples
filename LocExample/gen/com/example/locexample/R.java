086e55cc128e16d94b7cfb67eef419838dbe0052
Don Archer <don.archer@verizon.com>
2010-04-04 23:03:28 -0500

    modified Loc Example

--- LocExample/gen/com/example/locexample/R.java
+++ LocExample/gen/com/example/locexample/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int button=0x7f050001;
         public static final int text1=0x7f050000;
+        public static final int text2=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
